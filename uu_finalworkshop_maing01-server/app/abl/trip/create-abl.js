33caef7ff219a33234361b747c0a393513ca8efb
Dmitrii Kyrychenko <31820971+nyxeet@users.noreply.github.com>
2021-04-28 18:07:21 +0300

    fix create

--- uu_finalworkshop_maing01-server/app/abl/trip/create-abl.js
+++ uu_finalworkshop_maing01-server/app/abl/trip/create-abl.js
@@ -49,38 +49,40 @@ class TripAbl {
       ...dtoIn,
     };
 
-    let trip = null;
-    try {
-      trip = await this.dao.create(uuObject);
-    } catch (e) {
-      throw new Errors.Create.TripDaoCreateFailed({ uuAppErrorMap }, e);
-    }
     // HDS 3 image
     let uuBinary = null;
     if (dtoIn.image) {
       try {
-        uuBinary = await UuBinaryAbl.createBinary(awid, { data: dtoIn.image });
+        uuBinary = await UuBinaryAbl.createBinary(awid, {
+          data: dtoIn.image,
+        });
       } catch (e) {
         throw new Errors.Create.CreateBinaryFailed({ uuAppErrorMap }, e);
       }
       uuObject.userPic = uuBinary.code;
     }
-
     //HDS 4
+    try {
+      await this.locationDao.get(awid, dtoIn.locationId);
+    } catch (e) {
+      throw new Errors.Create.LocationDoesNotExist({ uuAppErrorMap }, e);
+    }
+    //HDS 5
     const participantsList = uuObject.participantList;
     const dbList = await this.participantDao.findMany(awid, participantsList);
     if (participantsList.length !== dbList.itemList.length) {
       throw new Errors.Create.ParticipantDoesNotExist({ uuAppErrorMap });
     }
 
-    //HDS 5
+    //HDS 6
+    let trip = null;
     try {
-      await this.locationDao.get(awid, dtoIn.locationId);
+      trip = await this.dao.create(uuObject);
     } catch (e) {
-      throw new Errors.Create.LocationDoesNotExist({ uuAppErrorMap }, e);
+      throw new Errors.Create.TripDaoCreateFailed({ uuAppErrorMap }, e);
     }
 
-    //HDS 6 - return
+    //HDS 7 - return
     return {
       ...trip,
       uuAppErrorMap,
